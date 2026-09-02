c92a234629f67d492abd8c01e84e81fe28018cab
mollyckane <214532456+mollyckane@users.noreply.github.com>
2026-09-02 13:27:08 +0100

    fix navigation on mobile display

--- src/components/layout/Navigation.jsx
+++ src/components/layout/Navigation.jsx
@@ -9,7 +9,7 @@ const navigationItems = [
 
 export default function Navigation() {
     return (
-        <nav className="bg-stone-100/30 backdrop-blur-md border-b border-stone-200 px-6 py-4 fixed left-0 top-0 z-50 w-full">
+        <nav className="bg-stone-100/30 backdrop-blur-md border-b border-stone-200 sm:px-6 px-1 py-4 fixed left-0 top-0 z-50 w-full">
             <ul className="items-center flex flex-wrap gap-1 divide-x divide-stone-300">
                 {navigationItems.map((item) => (
                     <li key={item.href} className="px-3 hover:text-stone-500 transition">
